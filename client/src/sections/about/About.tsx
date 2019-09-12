4261b6191a1da23532f968c44befbde01ef092f0
unknown <devlead@dynamisdevelopment.com>
2019-09-12 11:33:59 -0400

    Added About Section Wireframe

--- client/src/sections/about/About.tsx
+++ client/src/sections/about/About.tsx
@@ -6,6 +6,7 @@ const About = () => {
     return (
         <div>
             <SectionTitle title={'About'} subtitle={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'} />
+            <div className="about">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat at sed incidunt repudiandae necessitatibus unde iure officiis porro voluptatum, eius nobis rem tenetur corporis quasi autem iusto eos veniam delectus, asperiores ut. <br /> <br />Esse dolore, soluta, voluptas eos fugit reprehenderit, similique alias molestias quaerat distinctio ad. Quae esse ex debitis asperiores?</div>
         </div>
     )
 }
