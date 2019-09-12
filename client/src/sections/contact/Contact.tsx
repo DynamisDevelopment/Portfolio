c09ecb1b7c25514f90545ed2b9ecbfcdf2300619
unknown <devlead@dynamisdevelopment.com>
2019-09-12 16:04:10 -0400

    Added Contact Section Wireframe

--- client/src/sections/contact/Contact.tsx
+++ client/src/sections/contact/Contact.tsx
@@ -3,9 +3,24 @@ import './Contact.sass'
 import SectionTitle from '../../components/sectionTitle/SectionTitle'
 
 const Contact = () => {
+    const getYear = (): number => new Date().getFullYear()
     return (
-        <div>
+        <div className='contact'>
             <SectionTitle title={"Contact"} subtitle={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'} />
+            <form className='contact-form'>
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Email" />
+                <textarea name="message" placeholder="Message" />
+                <button type="submit">Submit</button>
+            </form>
+            <div className="social">
+                <a href="#" className="circle"><img src="../assets/icons/github-logo.svg" alt="Github Logo" /></a>
+                <a href="#" className="circle"><img src="../assets/icons/github-logo.svg" alt="Github Logo" /></a>
+                <a href="#" className="circle"><img src="../assets/icons/github-logo.svg" alt="Github Logo" /></a>
+                <a href="#" className="circle"><img src="../assets/icons/github-logo.svg" alt="Github Logo" /></a>
+                <a href="#" className="circle"><img src="../assets/icons/github-logo.svg" alt="Github Logo" /></a>
+            </div>
+            <div className="copyright">Made by yours truly © {getYear()}</div>
         </div>
     )
 }
