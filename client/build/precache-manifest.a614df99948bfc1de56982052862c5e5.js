b14a9ee999b7701f326b8557bdcd003a4b935b3a
unknown <devlead@dynamisdevelopment.com>
2019-09-15 18:23:41 -0400

    Added Build script

---
+++ client/build/precache-manifest.a614df99948bfc1de56982052862c5e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41c7f190eaf269567c6862b6a573d45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23733f5ca9af05943e0e",
+    "url": "/static/css/main.a9737934.chunk.css"
+  },
+  {
+    "revision": "706d29fcba0dd6bc1036",
+    "url": "/static/js/2.0740f0c1.chunk.js"
+  },
+  {
+    "revision": "23733f5ca9af05943e0e",
+    "url": "/static/js/main.c0d598e2.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
