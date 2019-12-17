ac2288f4acca3bf0e62dda8905a9b888a7da5a99
unknown <devlead@dynamisdevelopment.com>
2019-12-17 15:33:34 -0500

    misc

--- client/src/sections/portfolio/data.tsx
+++ client/src/sections/portfolio/data.tsx
@@ -19,36 +19,4 @@ export const projects: Props[] = [{
     tech: ['React', 'Gatsby', 'Contentful'],
     complete: true,
     id: 1
-},
-// {
-//     image: '../assets/projects/happy_paws.jpg',
-//     name: 'Happy Paws',
-//     desc: 'Branding page and blog for a pet store and adoption service.',
-//     link: '',
-//     git: 'https://github.com/DynamisDevelopment/Happy-Paws.git',
-//     designBy: 'ThemesPlace',
-//     tech: ['Gatsby', 'React', 'Wordpress', 'WooCommerce'],
-//     complete: false,
-//     id: 2
-// }, {
-// image: '../assets/projects/qertsa.jpg',
-// name: 'Qertsa',
-// desc: 'A native Android chat app for sharing messages, photos, and videos.  ',
-// link: '',
-// git: '',
-// designBy: 'Ogwebsolutions',
-// tech: ['Flutter', 'Firebase'],
-// complete: false,
-// id: 3
-//}, 
-{
-    image: '../assets/projects/itmemaths.jpg',
-    name: 'ItMeMaths',
-    desc: 'A math education site for discussion and resources.',
-    link: 'https://itmemaths.netlify.com/',
-    git: 'https://github.com/DynamisDevelopment/ItMeMaths',
-    designBy: 'Myself',
-    tech: ['React', 'Gatsby', 'Firebase', 'GraphQL', 'GraphCMS'],
-    complete: false,
-    id: 2
 }]
