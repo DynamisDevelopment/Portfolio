6900a3dc9ba5e994b46dadc1cefc9f754e5cec56
Joshua Hall <49533903+DynamisDevelopment@users.noreply.github.com>
2021-06-25 18:09:24 -0400

    Added codepen embeds

--- client/src/sections/about/About.tsx
+++ client/src/sections/about/About.tsx
@@ -53,10 +53,18 @@ const About = () => {
           <button className="btn">Dynamis Development</button>
         </a>
 
+        <div className="subtitle" style={{ maxWidth: "500px" }}>Recent Pens</div>
+
+        <div className="responsive-grid">
+          <iframe className='pen' scrolling="no" width="100%" title="Sine Waves" src="https://codepen.io/dynamisdevelopment/embed/rXpbyq?defaultTab=result&theme-id=dark"></iframe>
+          <iframe className='pen' scrolling="no" width="100%" title="Star Shower" src="https://codepen.io/dynamisdevelopment/embed/BXmvLd?defaultTab=result&theme-id=dark"></iframe>
+        </div>
+
         <div className="subtitle" style={{ maxWidth: "500px" }}>I have recently taken a interest in building VR games, 3D modelling, and leather working.</div>
         <Instagram />
-      </div>
-    </section>
+
+      </div >
+    </section >
   )
 }
 
@@ -79,7 +87,7 @@ const Reviews = () => {
           <div>
             <p>
               "Josh is a talented, internally motivated frontend engineer who exemplifies strong technical thinking and goal oriented execution. It's been really great working with him. Strongly recommended for anyone who's looking for top notch work and a friendly colleague." - Zack Peng (CEO of Kover.ai)
-          </p>
+            </p>
           </div>
           <div>
             <p>
