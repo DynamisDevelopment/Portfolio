3d29f6a16d83a05c0d2a6560275488dc76fbb9b4
Joshua Hall <49533903+DynamisDevelopment@users.noreply.github.com>
2021-07-15 07:20:21 -0400

    Added NPM link to Footer

--- client/src/sections/contact/Contact.tsx
+++ client/src/sections/contact/Contact.tsx
@@ -1,18 +1,16 @@
-import React from "react"
-import "./Contact.sass"
-import SectionTitle from "../../components/sectionTitle/SectionTitle"
-import axios from "axios"
-import { withFormik, Form, Field } from "formik"
-import * as Yup from "yup"
+import React from 'react'
+import './Contact.sass'
+import SectionTitle from '../../components/sectionTitle/SectionTitle'
+import axios from 'axios'
+import { withFormik, Form, Field } from 'formik'
+import * as Yup from 'yup'
 
 const App = ({ link, icon }) => {
-  return <a
-    href={link}
-    className="circle"
-    target="_blanck"
-  >
-    <img src={icon} alt="Linkedin Logo" />
-  </a>
+  return (
+    <a href={link} className="circle" target="_blanck">
+      <img src={icon} alt="Linkedin Logo" />
+    </a>
+  )
 }
 
 const Contact = () => {
@@ -22,17 +20,36 @@ const Contact = () => {
   return (
     <div className="contact" id="Contact">
       <SectionTitle
-        title={"Contact"}
-        subtitle={"Got something interesting?"}
-        email={"dynamisdev3248@gmail.com"}
+        title={'Contact'}
+        subtitle={'Got something interesting?'}
+        email={'dynamisdev3248@gmail.com'}
       />
       <FormikForm />
       <div className="social">
-        <App link="https://www.linkedin.com/in/joshua-hall-51b182185/" icon="../assets/icons/linkedin-logo.svg" />
-        <App link="https://www.upwork.com/o/profiles/users/_~013e4465c7ba054054/" icon="../assets/icons/upwork-logo.svg" />
-        <App link="https://codepen.io/dynamisdevelopment" icon="../assets/icons/codepen-logo.svg" />
-        <App link="https://github.com/DynamisDevelopment" icon="../assets/icons/github-logo.svg" />
-        <App link="https://hub.docker.com/u/dynamisdevelopment" icon="../assets/icons/docker-logo-white.svg" />
+        <App
+          link="https://www.linkedin.com/in/joshua-hall-51b182185/"
+          icon="../assets/icons/linkedin-logo.svg"
+        />
+        <App
+          link="https://www.upwork.com/o/profiles/users/_~013e4465c7ba054054/"
+          icon="../assets/icons/upwork-logo.svg"
+        />
+        <App
+          link="https://codepen.io/dynamisdevelopment"
+          icon="../assets/icons/codepen-logo.svg"
+        />
+        <App
+          link="https://github.com/DynamisDevelopment"
+          icon="../assets/icons/github-logo.svg"
+        />
+        <App
+          link="https://hub.docker.com/u/dynamisdevelopment"
+          icon="../assets/icons/docker-logo-white.svg"
+        />
+        <App
+          link="https://www.npmjs.com/~dynamisdevelopment"
+          icon="../assets/icons/npm-logo.svg"
+        />
       </div>
       <div className="copyright">Made by yours truly © {getYear()}</div>
     </div>
@@ -71,36 +88,36 @@ const ContactForm = ({ errors, touched }) => {
 const FormikForm = withFormik({
   mapPropsToValues() {
     return {
-      name: "",
-      email: "",
-      message: ""
+      name: '',
+      email: '',
+      message: '',
     }
   },
   validationSchema: Yup.object().shape({
-    name: Yup.string().required("A name is required"),
+    name: Yup.string().required('A name is required'),
     email: Yup.string()
-      .email("Please use a valid email")
-      .required("An email is required"),
+      .email('Please use a valid email')
+      .required('An email is required'),
     message: Yup.string()
       .min(
         25,
-        "Message must be more than 25 characters long, be descriptive :)"
+        'Message must be more than 25 characters long, be descriptive :)'
       )
-      .required("A message is required")
+      .required('A message is required'),
   }),
   handleSubmit(values, { resetForm }) {
     axios({
-      method: "POST",
-      url: "/.netlify/functions/server",
+      method: 'POST',
+      url: '/.netlify/functions/server',
       data: {
         name: values.name,
         email: values.email,
-        message: values.message
-      }
+        message: values.message,
+      },
     })
       .then(res => resetForm())
-      .catch(e => console.log("Error: ", e))
-  }
+      .catch(e => console.log('Error: ', e))
+  },
   // @ts-ignore
 })(ContactForm)
 
