a5a3b45e1c61d0f1e726c8e3ca9ad6e3064bacbf
Joshua Hall <49533903+DynamisDevelopment@users.noreply.github.com>
2022-06-23 14:25:25 -0400

    Added resume button

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from "react"
-import "./Navbar.sass"
-import { animated, useTrail, useSpring, config } from "react-spring"
-import withSizes from "react-sizes"
+import React, { useState, useEffect } from 'react'
+import './Navbar.sass'
+import { animated, useTrail, useSpring, config } from 'react-spring'
+import withSizes from 'react-sizes'
+import { Button } from '@chakra-ui/core'
 
 // @ts-ignore
 const Navbar = ({ show, isMobile }: any) => {
   const links: { name: string }[] = [
-    { name: "Services" },
-    { name: "Portfolio" },
-    { name: "Articles" },
-    { name: "About" },
-    { name: "Contact" },
+    { name: 'Services' },
+    { name: 'Portfolio' },
+    { name: 'Articles' },
+    { name: 'About' },
+    { name: 'Contact' },
   ]
 
   // * Set Mobile Navigation behavior
@@ -18,34 +19,52 @@ const Navbar = ({ show, isMobile }: any) => {
 
   // * Have each link swoop in from the upper right
   const swoop = useTrail(links.length, {
-    from: { transform: "translate(10px, -50px)" },
-    to: { transform: "translate(0, 0)" },
+    from: { transform: 'translate(10px, -50px)' },
+    to: { transform: 'translate(0, 0)' },
   })
 
   // * Set navbar state based on screen position
   const full = useSpring({
     to: {
-      backgroundColor: show || mobile ? "#fff" : "rgba(0,0,0,0)",
-      color: show || mobile ? "black" : "#fff",
+      backgroundColor: show || mobile ? '#fff' : 'rgba(0,0,0,0)',
+      color: show || mobile ? 'black' : '#fff',
     },
     config: config.slow,
   })
 
   // * Use mobile behavior on mobile, else use an empty spring and show links. Inelegant but effective ¯\_(ツ)_/¯
-  const hide = useSpring({ to: { display: mobile ? "flex" : "none" } })
-  const noHide = useSpring({ to: { color: show ? "rgba(0,0,0,1)" : "#fff" } })
+  const hide = useSpring({ to: { display: mobile ? 'flex' : 'none' } })
+  const noHide = useSpring({ to: { color: show ? 'rgba(0,0,0,1)' : '#fff' } })
 
   // * Turn off remove mobile nav if resized to desktop
   useEffect(() => {
     if (!isMobile) toggle(false)
   })
+
+  const btnHover = {
+    bg: show || mobile ? '#6628D5' : '#fff',
+    color: show || mobile ? '#fff' : '#6628D5',
+  }
   return (
     <animated.div className="navbar" style={full}>
       <div className="row">
-        <a href="https://www.dynamisdevelopment.com/" target="_blank" rel="noreferrer">
-          <img src="../assets/icons/dynamis-logo-white.svg" alt="Dynamis Development Logo" className="profile-pic" />
+        <a
+          href="https://www.dynamisdevelopment.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="../assets/icons/dynamis-logo-white.svg"
+            alt="Dynamis Development Logo"
+            className="profile-pic"
+          />
         </a>
-        <img src="../assets/icons/burger.svg" alt="Navigation Burger" className="burger" onClick={() => toggle(!mobile)}/>
+        <img
+          src="../assets/icons/burger.svg"
+          alt="Navigation Burger"
+          className="burger"
+          onClick={() => toggle(!mobile)}
+        />
       </div>
 
       <animated.ul className="links" style={isMobile ? hide : noHide}>
@@ -59,11 +78,28 @@ const Navbar = ({ show, isMobile }: any) => {
             <a href={`#${links[index].name}`}> {links[index].name} </a>
           </animated.li>
         ))}
+        <animated.li onClick={() => toggle(!mobile)}>
+          {' '}
+          <a href={'../assets/resume.pdf'} download>
+            <Button
+              onClick={() => {}}
+              bg={show || mobile ? '#6628D5' : '#fff'}
+              color={show || mobile ? '#fff' : '#6628D5'}
+              _hover={btnHover}
+              _active={btnHover}
+              _focus={btnHover}
+              w={['90vw', '90vw', 'auto']}
+            >
+              {' '}
+              Resume
+            </Button>
+          </a>
+        </animated.li>
       </animated.ul>
     </animated.div>
   )
 }
 
-const mapSizesToProps = ({ width }) => ({ isMobile: width < 650 })
+const mapSizesToProps = ({ width }) => ({ isMobile: width < 950 })
 
 export default withSizes(mapSizesToProps)(Navbar)
